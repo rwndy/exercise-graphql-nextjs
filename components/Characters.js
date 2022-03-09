aca3187345de8cb1861cdf34edcf28d3c88ab0a7
rwndy <riwandy10@gmail.com>
2022-03-09 20:08:41 +0700

    git init

---
+++ components/Characters.js
@@ -0,0 +1,33 @@
+import React from "react"
+import Image from "next/image"
+import { Heading, Text, SimpleGrid } from "@chakra-ui/react"
+
+const Characters = ({characters}) => {
+  return (
+    <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+      {
+        characters && characters.map(character => (
+          <div key={character?.id}>
+            <Image
+              src={character?.image}
+              width={300}
+              height={300}
+              alt={character?.name}
+            />
+            <Heading as="h4" textAlign={"center"} size="md">
+              {character?.name}
+            </Heading>
+            <Text align={"center"} >
+              Origin: {character?.origin?.name}
+            </Text>
+            <Text align={"center"} >
+              Location: {character?.location?.name}
+            </Text>
+          </div>
+        ))
+      }
+    </SimpleGrid>
+  )
+}
+
+export default Characters
